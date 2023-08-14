b6cee24771f65e37cce0f8689c84b441b8477966
K.Yosuke <117277631+aqyuki@users.noreply.github.com>
2023-08-15 07:23:38 +0900

    feat : レーダチャートを表示するように変更 (#5)
    * chore : 依存パッケージを追加
    
    * chore : propsのタイプを定義
    
    * feat : レーダーチャートを追加
    
    * feat : レーダチャートのコンポーネントを追加
    
    * fix : 要素名が見切れていた問題を修正

---
+++ components/radar.tsx
@@ -0,0 +1,58 @@
+import {
+  PolarAngleAxis,
+  PolarGrid,
+  PolarRadiusAxis,
+  Radar,
+  RadarChart,
+} from "recharts";
+
+export const ResultRadarChart = (props: ResultRadarChartProps) => {
+  const data = [
+    {
+      title: "Line",
+      score: props.line_count,
+      fullMark: 2,
+    },
+    {
+      title: "Max indent size",
+      score: props.indent_size,
+      fullMark: 2,
+    },
+    {
+      title: "Token count",
+      score: props.token_count,
+      fullMark: 2,
+    },
+    {
+      title: "Token length",
+      score: props.token_length,
+      fullMark: 2,
+    },
+    {
+      title: "Function count",
+      score: props.function_count,
+      fullMark: 2,
+    },
+  ];
+  return (
+    <RadarChart
+      cx={300}
+      cy={250}
+      outerRadius={150}
+      width={600}
+      height={500}
+      data={data}
+    >
+      <PolarGrid />
+      <PolarAngleAxis dataKey={"title"} />
+      <PolarRadiusAxis />
+      <Radar
+        name="Score"
+        dataKey={"score"}
+        stroke="#8884d8"
+        fill="#add8e6"
+        fillOpacity={0.6}
+      />
+    </RadarChart>
+  );
+};
---
+++ types/radar.d.ts
@@ -0,0 +1,7 @@
+interface ResultRadarChartProps {
+  line_count: number;
+  indent_size: number;
+  token_count: number;
+  token_length: number;
+  function_count: number;
+}
