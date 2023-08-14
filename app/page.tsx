eff9f50658dce1a20fdd3b5de16c72ce77ea53cc
K.Yosuke <117277631+aqyuki@users.noreply.github.com>
2023-08-14 21:08:45 +0900

    feat : トップページを更新 (#2)
    * feat : Topページを更新
    
    * chore : 不要なパッケージを削除
    
    * chore : エディターの設定フォルダがGitでの追跡に含まれないように
    
    * chore : サブモジュールを追加
    
    * feat : 押したボタンによって表示するモーダルを切り替える機能を実装
    
    * feat : ボタンクリック時にモーダルが表示されるように変更
    
    * feat : ルームを操作するモーダルを作成
    
    * chore : props名を変更
    
    * feat : propsの型を追加
    
    * fix : 入力されたIDが正常に保存されない問題を修正
    
    * chore : 不要になったコードを削除
    
    * chore : change function name
    
    * feat : 表示テキストを変更
    
    * chore : 不要なインポートを削除
    
    * chore : ボタンのテキストを変更
    
    * chore : テキストを変更
    
    * chore : 簡略化できる部分を簡略化

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,45 @@
 "use client";
 
-import { Heading } from "@chakra-ui/react";
+import {
+  Heading,
+  Text,
+  Center,
+  Button,
+  Container,
+  useDisclosure,
+} from "@chakra-ui/react";
+import { ReadyModal } from "@/components/modal";
 
 export default function Home() {
+  // モーダルの状態を管理するためのフック
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Heading>Code Connect</Heading>
-    </main>
+    <>
+      <Container maxW={"3xl"}>
+        <Center>
+          <Heading as={"h1"} size={"3xl"}>
+            Code Connect
+          </Heading>
+        </Center>
+        <Center>
+          <Text className="my-10" fontSize="2xl">
+            Code
+            Connectのコード診断では、テーマに沿ってコードを書いてもらいます。書いてもらったコードからあなたのプログラミングタイプを診断します。
+          </Text>
+        </Center>
+        <Center>
+          <Text className="my-10" fontSize="2xl">
+            "プレイ" ボタンからプレイを開始できます。あなたのタイプは一体…
+          </Text>
+        </Center>
+        <Center>
+          <Button colorScheme="blue" variant={"outline"} onClick={onOpen}>
+            プレイ
+          </Button>
+        </Center>
+      </Container>
+      <ReadyModal IsOpen={isOpen} OnClose={onClose} />
+    </>
   );
 }
---
+++ components/modal.tsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import {
+  Modal,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalCloseButton,
+  ModalFooter,
+  ModalOverlay,
+} from "@chakra-ui/react";
+
+import { Button, ButtonGroup } from "@chakra-ui/react";
+import { Text, Divider } from "@chakra-ui/react";
+import { ModalProps } from "@/types/modal";
+
+// CreateRoomModalContent・JoinRoomModalContentはModalに表示するコンテンツを返す。
+// Modalの表示・非表示は、呼び出し側のStateで管理するためここには書かない。
+
+export const ReadyModal = (props: ModalProps) => {
+  // ルームのIDを保存するState
+  const [roomID, setRoomID] = useState("0000");
+
+  // TODO: サーバーにルームの作成を要求する処理を非同期で呼び出す。
+  return (
+    <Modal isOpen={props.IsOpen} onClose={props.OnClose}>
+      <ModalOverlay />
+      <ModalContent width={"3xl"}>
+        <ModalHeader>プログラミング タイプ診断</ModalHeader>
+        <ModalCloseButton />
+        <Divider />
+        <ModalBody>
+          <Text fontSize={"md"}>プログラミングタイプの診断を始めますか？</Text>
+        </ModalBody>
+        <ModalFooter>
+          <ButtonGroup variant="outline" spacing={6}>
+            <Button colorScheme="red" onClick={props.OnClose}>
+              やめる
+            </Button>
+            <Button colorScheme="blue">始める</Button>
+          </ButtonGroup>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
+};
