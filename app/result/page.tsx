a5f800b8914e015d692b87a45b64192134b3543e
yksk171118@gmail.com <yksk171118@gmail.com>
2023-08-14 22:43:27 +0900

    feat : リザルト画面を作成

--- app/result/page.tsx
+++ app/result/page.tsx
@@ -1,5 +1,22 @@
 "use client";
 
+import { Center, Container, Heading, VStack } from "@chakra-ui/react";
+
 export default function Result() {
-  return <>Result</>;
+  return (
+    <>
+      <Container maxW={"3xl"}>
+        <Center>
+          <VStack spacing={"10"}>
+            <Heading as={"h1"} size={"3xl"}>
+              Code Connect
+            </Heading>
+            <Heading as={"h2"} fontSize={"2xl"}>
+              ✨診断結果
+            </Heading>
+          </VStack>
+        </Center>
+      </Container>
+    </>
+  );
 }
