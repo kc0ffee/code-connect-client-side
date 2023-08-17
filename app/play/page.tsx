8dd8207e5d3ae15d9488e5d38080dcf8bb56b21f
yksk171118@gmail.com <yksk171118@gmail.com>
2023-08-18 08:09:25 +0900

    chore : APIの呼び出しを追加

--- app/play/page.tsx
+++ app/play/page.tsx
@@ -15,9 +15,14 @@ import {
   ModalContent,
   ModalHeader,
   ModalFooter,
+  RadioGroup,
+  Stack,
+  Radio,
 } from "@chakra-ui/react";
 import Link from "next/link";
-import { useState } from "react";
+import React, { useEffect, useState, useContext } from "react";
+import { CodeApi, Configuration, Theme, ThemeApi } from "@/api";
+import { CodeDataContext } from "@/providers/code_data_context";
 
 export default function Play() {
   // 表示するモーダルの一覧
@@ -26,20 +31,78 @@ export default function Play() {
     submit: "submit",
   };
 
-  const [theme, setTheme] = useState("プログラム テーマ");
-  const [showModal, setShowModal] = useState(ShowModal.submit);
-  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [theme, setTheme] = useState<Theme>({
+    id: -1,
+    theme: "Load failure",
+  }); // manage theme
+  const [language, setLanguage] = useState("go"); // 使用言語
+  const [code, setCode] = useState(""); // 作成コード
+  const [showModal, setShowModal] = useState(ShowModal.submit); // manage show modal
+  const { isOpen, onOpen, onClose } = useDisclosure(); // manage modal status
+  const CodeData = useContext(CodeDataContext); // コードをサーバーに送信した結果を表示する
 
+  // テーマをAPIから取得するs
+  const GetThemeAsync = async () => {
+    const conf = new Configuration({
+      basePath: process.env.NEXT_PUBLIC_API_THEME_URL,
+    });
+    await new ThemeApi(conf)
+      .apiThemeGet()
+      .then((res) => {
+        setTheme(res);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
+
+  // コードを送信する
+  const SendCode: any = async () => {
+    const conf = new Configuration({
+      basePath: process.env.NEXT_PUBLIC_API_CODE_URL,
+    });
+    await new CodeApi(conf)
+      .apiCodePost({
+        postBody: {
+          themeId: 1,
+          language: language,
+          code: Buffer.from(code).toString("base64"),
+        },
+      })
+      .then((res) => {
+        CodeData.code = code;
+        CodeData.id = res.id;
+        CodeData.themeId = res.themeId;
+        CodeData.timeStamp = res.timeStamp;
+      })
+      .catch((e) => {
+        console.error(e);
+        return;
+      });
+  };
+
+  // キャンセル確認のポップアップを表示する
   const ShowCancelModal = () => {
     setShowModal(ShowModal.cancel);
     onOpen();
   };
+
+  // 送信確認のポップアップを表示する
   const ShowSubmitModal = () => {
     setShowModal(ShowModal.submit);
     onOpen();
   };
 
-  // TODO: APIからデータを取ってくる処理を入れる。
+  // テキストエリアが変更されたときのハンドラー
+  const TextareaChangeHandler = (e: any) => {
+    const value = e.target.value;
+    setCode(value);
+  };
+
+  // Initialize
+  useEffect(() => {
+    GetThemeAsync();
+  }, []);
 
   return (
     <>
@@ -51,11 +114,28 @@ export default function Play() {
         </Center>
         <Center className="my-10">
           <Heading as={"h2"} fontSize={"2xl"}>
-            {theme}
+            {theme?.theme}
           </Heading>
         </Center>
         <Center>
-          <Textarea placeholder="Write your code" height={"2xl"} />
+          <RadioGroup onChange={setLanguage} value={language}>
+            <Stack direction={"row"}>
+              <Radio value="go" size="lg">
+                Go
+              </Radio>
+              <Radio value="python" size="lg">
+                Python
+              </Radio>
+            </Stack>
+          </RadioGroup>
+        </Center>
+        <Center className="my-5">
+          <Textarea
+            placeholder="Write your code"
+            value={code}
+            height={"2xl"}
+            onChange={TextareaChangeHandler}
+          />
         </Center>
         <Center>
           <ButtonGroup variant={"outline"} className="my-10" spacing={"10"}>
@@ -104,7 +184,9 @@ export default function Play() {
                     <Link href={"/"}> 回答をやめる</Link>
                   </Button>
                 ) : (
-                  <Button colorScheme="blue">送信する</Button>
+                  <Button colorScheme="blue" onClick={SendCode}>
+                    <Link href={"/result"}>送信する</Link>
+                  </Button>
                 )}
               </ButtonGroup>
             </Center>
