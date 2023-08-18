99784d14eb1183b304f200b4e3e0574559ccc4d7
Raiga SASAYAMA <raiga0310brilio@gmail.com>
2023-08-19 06:53:46 +0900

    feat: 簡易な説明文を追加

--- components/typeResult.tsx
+++ components/typeResult.tsx
@@ -24,7 +24,7 @@ return (
           {props.type}
         </Heading>
         <Text py="2">
-          あなたのプログラミングタイプは{props.type}です。説明を追加説明を追加説明を追加説明を追加説明を追加説明を追加説明を追加説明を追加説明を追加説明を追加説明を追加説明を追加説明を追加説明を追加説明を追加説明を追加説明を追加説明を追加説明を追加説明を追加説明を追加説明を追加説明を追加説明を追加説明を追加説明を追加説明を追加説明を追加説明を追加説明を追加説明を追加
+          あなたのプログラミングタイプは{props.type}です。{typeDescription(props.type)}
         </Text>
       </CardBody>
     </Stack>
@@ -33,27 +33,40 @@ return (
 }
 
 const TypeColor = (type: string) => {
-    switch (type) {
-        case "analyst":
-            return "#3498db";
-        case "gatekeeper":
-            return "#27ae60";
-        case "diplomat":
-            return "#e67e22";
-        case "artist":
-            return "#d35400";
-    }
+  switch (type) {
+    case "analyst":
+      return "#3498db";
+    case "gatekeeper":
+      return "#27ae60";
+    case "diplomat":
+      return "#e67e22";
+    case "artist":
+      return "#d35400";
+}
+}
+
+const typeDescription = (type: string) => {
+  switch (type) {
+    case "analyst":
+      return "深い見識と論理的思考を持ち、情報の精緻な解剖を行います。現実的で緻密なアプローチが特長です。";
+    case "gatekeeper":
+      return "豊かな経験と洞察力で、状況の異変を察知・監視します。安定と秩序の維持に尽力します。";
+    case "diplomat":
+      return "柔軟なコミュニケーション力で対話を奨励し、調和を築きます。交渉と協力の才を持っています。";
+    case "artist":
+      return "創造力溢れる発想で新たな世界を生み出し、感情を鮮やかに表現します。美と独創性を追求します。";
+  }
 }
 
 const TypeImage = (type: string) => {
-    switch (type) {
-        case "analyst":
-            return {src: "/vercel.svg"};
-        case "gatekeeper":
-            return {src: "/gatekeeper.svg"};
-        case "diplomat":
-            return {src: "/diplomat.svg"};
-        case "artist":
-            return {src: "/artist.svg"};
-    }
+  switch (type) {
+    case "analyst":
+      return {src: "/vercel.svg"};
+    case "gatekeeper":
+      return {src: "/gatekeeper.svg"};
+    case "diplomat":
+      return {src: "/diplomat.svg"};
+    case "artist":
+      return {src: "/artist.svg"};
+  }
 }
