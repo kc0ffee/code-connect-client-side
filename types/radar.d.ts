e72c4d8ba32536b8e0b11f0772e58f8763c34afb
yksk171118@gmail.com <yksk171118@gmail.com>
2023-08-18 13:23:58 +0900

    chore : 不要になったプロパティを削除

--- types/radar.d.ts
+++ types/radar.d.ts
@@ -1,7 +1,6 @@
 interface ResultRadarChartProps {
   line_count: number;
-  indent_size: number;
   token_count: number;
   token_length: number;
-  function_count: number;
+  function_len: number;
 }
